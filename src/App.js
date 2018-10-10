1d2f4ee86d6183948e7437a4a49321c0042faf02
Yury Savin <savinyurii@gmail.com>
2018-10-10 09:33:36 +0300

    add webvisor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { YMInitializer } from 'react-yandex-metrika';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { YMInitializer } from "react-yandex-metrika";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -13,7 +13,16 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
         </header>
-        <YMInitializer accounts={[12773485]} version="2"/>
+        <YMInitializer
+          accounts={[12773485]}
+          options={{
+            webvisor: true,
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true
+          }}
+          version="2"
+        />
       </div>
     );
   }
